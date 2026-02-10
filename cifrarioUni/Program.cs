b7b3b07a249161c6bbbe138e686b0554c3f873f4
belladonna.giacomo <belladonna.giacomo@HP-009>
2026-02-10 10:12:02 +0100

    Aggiungere i file di progetto.

---
+++ cifrarioUni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cifrarioUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
