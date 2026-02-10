2d04ff08d0144689882a57a90e4d567a55992f0b
belladonna.giacomo <belladonna.giacomo@HP-009>
2026-02-10 10:46:04 +0100

    mod

--- cifrarioUni/Program.cs
+++ cifrarioUni/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static void Cifrario(char[] alfabeto, string frase, string parola)
+        {
+            char[] f = frase.ToCharArray();
+            char[] p = parola.ToCharArray();
+            for(int i = 0;  i < f.Length; i++)
+            {
+               for(int j = 0; j < p.Length; j++)
+               {
+                    Console.Write(p[j]);
+               }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[] alfabeto = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            Console.WriteLine("Dammi una frase ");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Dammi una parola ");
+            string parola = Console.ReadLine();
+            Cifrario(alfabeto, frase, parola);
         }
     }
 }
